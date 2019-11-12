a3c302d5b9e5a77cce620ac58a6b814039ecdbc9
NishantSingh <55411341+nishantsingh9084@users.noreply.github.com>
2019-11-12 23:53:41 +0530

    WAP for declaration of variable

---
+++ variable_declaration.php
@@ -0,0 +1,13 @@
+/*Creating (Declaring) PHP Variables
+In PHP, a variable starts with the $ sign, followed by the name of the variable:*/
+
+<?php
+$txt = "Hello world!";
+$x = 5;
+$y = 10.5;
+?>
+
+/* output
+Hello world!
+5
+10.5                    */
