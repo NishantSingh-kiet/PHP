57171caf41a14b80aa1da9dad322c13085443c3f
NishantSingh <55411341+nishantsingh9084@users.noreply.github.com>
2019-11-13 01:26:37 +0530

    WAP to calculate length,reverse and word count in php

---
+++ strings.php
@@ -0,0 +1,32 @@
+/*PHP String Functions
+
+
+strlen() - Return the Length of a String
+The PHP strlen() function returns the length of a string.
+
+Example
+Return the length of the string "Hello world!":*/
+
+<?php
+echo strlen("Hello world!"); // outputs 12
+?>
+
+/* str_word_count() - Count Words in a String
+The PHP str_word_count() function counts the number of words in a string.
+
+Example
+Count the number of word in the string "Hello world!":*/
+
+<?php
+echo str_word_count("Hello world!"); // outputs 2
+?>
+
+/*strrev() - Reverse a String
+The PHP strrev() function reverses a string.
+
+Example
+Reverse the string "Hello world!":*/
+
+<?php
+echo strrev("Hello world!"); // outputs !dlrow olleH
+?>
